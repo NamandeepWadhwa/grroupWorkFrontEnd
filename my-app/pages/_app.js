219e9cf345c8adf4c16a7475531b028b9127fbdf
SangWoo-Shin <90230423+SangWoo-Shin@users.noreply.github.com>
2024-06-19 22:07:46 -0400

    Update _app.js

--- my-app/pages/_app.js
+++ my-app/pages/_app.js
@@ -3,12 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import RouteGuard from "@/components/RouteGuard";
 import { Container } from "react-bootstrap";
 import Layout from "@/components/Layout";
-
+import style from "@/styles/Home.module.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <RouteGuard>
-      <Container>
+      <Container className={style.mainContainer}>
         <Layout>
           <Component {...pageProps} />
         </Layout>
