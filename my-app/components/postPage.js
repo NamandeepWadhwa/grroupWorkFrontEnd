93e4c343446e44f9a08ef2bf25b00eb02a5e5509
SangWoo-Shin <90230423+SangWoo-Shin@users.noreply.github.com>
2024-06-20 23:58:40 -0400

    Update postPage.js

--- my-app/components/postPage.js
+++ my-app/components/postPage.js
@@ -7,12 +7,13 @@ import { Button } from 'react-bootstrap';
 import { FaArrowLeft } from 'react-icons/fa';
 import styles from '@/styles/Home.module.css';
 import styles1 from '@/styles/CreateActivity.module.css';
+import Comments from '@/components/comments';
 
 const PostPage = ({ postInfo }) => {
   const router = useRouter();
+
   useEffect(() => {
     if (!postInfo) return;
-
   }, [postInfo]);
 
   if (!postInfo) return null; 
@@ -27,44 +28,47 @@ const PostPage = ({ postInfo }) => {
         await axios.delete(`${process.env.NEXT_PUBLIC_BACKENDURL}/freeboard/${postInfo._id}`);
         router.push(`/freeboard`);
       } catch (error) {
-        console.error('Error deleting post :', error);
+        console.error('Error deleting post:', error);
       }
     }
   };
 
   return (
     <>
-    <div>
-      <div className="d-flex align-items-center">
-        <div className="w-75 justify-content-start">
-          <Link href="/freeboard" passHref={true} legacyBehavior={true}>
-            <a className={styles1.backLink}>
-              <FaArrowLeft className={styles1.arrowIcon} />
-            </a>
-          </Link>
+      <div>
+        <div className="d-flex align-items-center">
+          <div className="w-75 justify-content-start">
+            <Link href="/freeboard" passHref={true} legacyBehavior={true}>
+              <a className={styles1.backLink}>
+                <FaArrowLeft className={styles1.arrowIcon} />
+              </a>
+            </Link>
+          </div>
+          <Button onClick={handleEditClick} className={styles.eBtn}>
+            Edit post
+          </Button>
+          <Button onClick={handleDelete} className={styles.dBtn}>
+            Delete post
+          </Button>
         </div>
-        <Button onClick={handleEditClick} className={styles.postBtn}>
-          Edit post
-        </Button>
-        <Button onClick={handleDelete} className={styles.postBtn}>
-          Delete post
-        </Button>
-      </div>
-      <div className={styles.freeboardContainer}>
-        <div className={styles.freeboard}>
-          <div className="d-flex">
-            <div className={styles.postHeader}>
-              <h1>{postInfo.title}</h1>
-              <time>{formatISO9075(new Date(postInfo.createdAt))}</time>
+        <div className={styles.freeboardContainer}>
+          <div className={styles.freeboard}>
+            <div className="d-flex">
+              <div className={styles.postHeader}>
+                <h1>{postInfo.title}</h1>
+                <time>{formatISO9075(new Date(postInfo.createdAt))}</time>
+              </div>
             </div>
+            <div className={styles.image}>
+              <img src={`${process.env.NEXT_PUBLIC_BACKENDURL}/${postInfo.image}`} alt="" />
+            </div>
+            <div className="content" dangerouslySetInnerHTML={{ __html: postInfo.content }} />
           </div>
-          <div className={styles.image}>
-            <img src={`${process.env.NEXT_PUBLIC_BACKENDURL}/${postInfo.image}`} alt=""/>
+          <div className={styles.commentsSection}>
+            <Comments postId={postInfo._id} />
           </div>
-          <div className="content" dangerouslySetInnerHTML={{ __html: postInfo.content }} />
         </div>
       </div>
-    </div>
     </>
   );
 };
