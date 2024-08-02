d3265d9dd25ce76a42bfe71d6f723d3c3b88c227
SangWoo-Shin <90230423+SangWoo-Shin@users.noreply.github.com>
2024-08-02 06:23:54 -0400

    Update post.js

--- my-app/pages/freeboard/post.js
+++ my-app/pages/freeboard/post.js
@@ -1,8 +1,17 @@
-import { format } from 'date-fns';
+import { Modal, Button, Card } from 'react-bootstrap';
+import { useState } from 'react';
 import Link from 'next/link';
+import { format } from 'date-fns';
 import styles from '@/styles/Home.module.css';
 
 const Post = ({ _id, title, image, createdAt, user }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleShowModal = () => setShowModal(true);
+  const handleCloseModal = () => setShowModal(false);
+
+  console.log('User data:', user);
+
   return (
     <div className={styles.post}>
       <div className="d-flex post">
@@ -18,12 +27,36 @@ const Post = ({ _id, title, image, createdAt, user }) => {
           <p className="info">
             <time>{format(new Date(createdAt), 'yyyy-MM-dd')}</time>
             {' | '}
-            {user && `${user.first_name} ${user.last_name}`}
+            {user && <span onClick={handleShowModal} style={{ cursor: 'pointer', color: '#007bff' }}>{`${user.first_name} ${user.last_name}`}</span>}
           </p>
         </div>
       </div>
+
+      {/* User Info Modal */}
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>User Profile</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Card className={styles.profileCard}>
+            <Card.Body>
+              <Card.Title>Profile</Card.Title>
+              <div className={styles.profilePicture}>
+                <img src={user.profile_picture} alt="User Icon" />
+              </div>
+              <Card.Text>
+                <strong>Name:</strong> {user.first_name} {user.last_name}<br />
+                <strong>Bio:</strong> {user.bio}<br />
+                <strong>Year of Entrance:</strong> {user.entrance_year}<br />
+                <strong>Program:</strong> {user.program}<br />
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 };
 
 export default Post;
+
