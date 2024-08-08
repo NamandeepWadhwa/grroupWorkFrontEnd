def95819471ee34cc42876737feaf56d72871085
Namandeep Singh Wadhwa <naman23156@gmail.com>
2024-08-07 22:16:49 -0400

    Avatar added for events, questions, asnwer and ticket

---
+++ my-app/components/Avatar.js
@@ -0,0 +1,35 @@
+// components/Avatar.js
+import React from "react";
+import { Image } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { gettingUser } from "@/lib/gettingUser";
+
+const Avatar = ({ id }) => {
+  useEffect(() => {
+    const token = gettingUser().token;
+    const fetchData = async () => {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BACKENDURL}/getUser`, {
+        method: "GET",
+        headers: {
+          Authorization: `JWT ${token}`,
+        },
+      });
+
+      const data = await res.json();
+      console.log(data);
+      setImage(data.profile_picture);
+      setUserName(data.first_name);
+    };
+    fetchData();
+  }, [id]);
+  const [image, setImage] = useState("/backArrowQuestions.png");
+  const [userName, setUserName] = useState("Unknown");
+  return (
+    <div className="d-flex align-items-center my-2">
+      <Image src={image} roundedCircle width={50} height={50} alt={userName} />
+      <span className="ms-2">{userName}</span>
+    </div>
+  );
+};
+
+export default Avatar;
